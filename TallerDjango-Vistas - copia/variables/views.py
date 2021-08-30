d9331940969e54ceb47f0e895876458835e79f19
andres <af.garciab@uniandes.edu.co>
2021-08-29 20:36:32 -0500

    lo que alcance

---
+++ TallerDjango-Vistas - copia/variables/views.py
@@ -0,0 +1,8 @@
+from .logic.logic_variables import get_all_variables
+from django.http import HttpResponse
+from django.core import serializers
+
+def get_variables (request):
+    variables = get_all_variables()
+    variables_list = serializers.serialize('json', variables)
+    return HttpResponse(variables_list, content_type='application/json')
